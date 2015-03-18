d7c8ea12947d91b0081af3990ee2a705761b4920
Pål <paalbjo@gmail.com>
2015-03-18 13:31:55 +0100

    sensor_innputs

---
+++ Pinns.h
@@ -0,0 +1,13 @@
+const int hallPin = 13; 
+const int hallPin2 = 12;
+const int hallPin3 = 11;
+const int hallPin4 = 10;
+//const int hallPin5 = 9;
+
+int hallState = 0;
+int hallState2 = 0;
+int hallState3 = 0;
+int hallState4 = 0 ;
+//int hallState5 = 0;
+
+ 
